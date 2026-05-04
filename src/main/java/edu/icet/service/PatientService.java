54269071033c6324a772f130049126684b248766
Rasanjali-Perera <chethanaperera0912@gmail.com>
2026-05-05 00:20:35 +0530

    Update PatientService interface to include CRUD operations and rename method for clarity

--- src/main/java/edu/icet/service/PatientService.java
+++ src/main/java/edu/icet/service/PatientService.java
@@ -5,5 +5,9 @@ import edu.icet.dto.PatientDto;
 import java.util.List;
 
 public interface PatientService {
-    List<PatientDto> getAllPatient();
+    List<PatientDto> getAllPatients();
+    boolean addPatient(PatientDto patientDto);
+    boolean updatePatient(PatientDto patientDto);
+    boolean deletePatient(Integer id);
+    PatientDto searchPatient(Integer id);
 }
