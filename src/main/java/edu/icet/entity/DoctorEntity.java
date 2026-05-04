659cb1515e66d9c76e501f46049320336a1793c6
Rasanjali-Perera <chethanaperera0912@gmail.com>
2026-05-05 00:30:30 +0530

    Add DoctorEntity class with JPA annotations

--- src/main/java/edu/icet/entity/DoctorEntity.java
+++ src/main/java/edu/icet/entity/DoctorEntity.java
@@ -1,13 +1,20 @@
-package edu.icet.dto;
+package edu.icet.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class DoctorDto {
+@Entity
+public class DoctorEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String specialization;
