9b69e348191e9736792330db3cf17da31588fb25
Rasanjali-Perera <chethanaperera0912@gmail.com>
2026-05-05 00:07:57 +0530

    Add PatientEntity class with fields and JPA annotations

---
+++ src/main/java/edu/icet/entity/PatientEntity.java
@@ -0,0 +1,25 @@
+package edu.icet.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class PatientEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String dateOfBirth;
+    private String gender;
+    private String email;
+    private String phone;
+    private String address;
+}
