8b33ab54f7e7b20c95c8c26297c87925e0e1a323
Rasanjali-Perera <chethanaperera0912@gmail.com>
2026-05-05 00:21:47 +0530

    Implement PatientService methods in PatientServiceImpl

--- src/main/java/edu/icet/service/impl/PatientServiceImpl.java
+++ src/main/java/edu/icet/service/impl/PatientServiceImpl.java
@@ -1,4 +1,34 @@
 package edu.icet.service.impl;
 
-public class PatientServiceImpl {
+import edu.icet.dto.PatientDto;
+import edu.icet.service.PatientService;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+public class PatientServiceImpl implements PatientService {
+    @Override
+    public List<PatientDto> getAllPatients() {
+        return List.of();
+    }
+
+    @Override
+    public boolean addPatient(PatientDto patientDto) {
+        return false;
+    }
+
+    @Override
+    public boolean updatePatient(PatientDto patientDto) {
+        return false;
+    }
+
+    @Override
+    public boolean deletePatient(Integer id) {
+        return false;
+    }
+
+    @Override
+    public PatientDto searchPatient(Integer id) {
+        return null;
+    }
 }
