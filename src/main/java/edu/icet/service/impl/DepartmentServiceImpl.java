9586bc7d47e4d559e9d58fc6afa15d14acdb95c1
Rasanjali-Perera <chethanaperera0912@gmail.com>
2026-05-04 22:42:54 +0530

    Add configuration for ModelMapper and implement DepartmentService methods

--- src/main/java/edu/icet/service/impl/DepartmentServiceImpl.java
+++ src/main/java/edu/icet/service/impl/DepartmentServiceImpl.java
@@ -1,4 +1,37 @@
 package edu.icet.service.impl;
 
-public class DepartmentServiceImpl {
+import edu.icet.dto.DepartmentDto;
+import edu.icet.service.DepartmentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class DepartmentServiceImpl implements DepartmentService {
+    @Override
+    public List<DepartmentDto> getAllDepartment() {
+        return List.of();
+    }
+
+    @Override
+    public boolean addDepartment(DepartmentDto departmentDto) {
+        return false;
+    }
+
+    @Override
+    public boolean updateDepartment(DepartmentDto departmentDto) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteDepartment(Integer id) {
+        return false;
+    }
+
+    @Override
+    public DepartmentDto searchDepartment(Integer id) {
+        return null;
+    }
 }
