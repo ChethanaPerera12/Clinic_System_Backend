c0352d100b455fc93895e7a134cbd4c7e6a8e241
Rasanjali-Perera <chethanaperera0912@gmail.com>
2026-05-06 16:52:46 +0530

    Update DepartmentController to use Departments model instead of DepartmentDto

--- src/main/java/edu/icet/controller/DepartmentController.java
+++ src/main/java/edu/icet/controller/DepartmentController.java
@@ -1,6 +1,6 @@
 package edu.icet.controller;
 
-import edu.icet.dto.DepartmentDto;
+import edu.icet.model.Departments;
 import edu.icet.service.DepartmentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -15,23 +15,23 @@ public class DepartmentController {
     private final DepartmentService departmentService;
 
     @GetMapping("/getAll")
-    public List<DepartmentDto> getAllDepartment(){
+    public List<Departments> getAllDepartment(){
         return departmentService.getAllDepartment();
     }
     @PostMapping("/add")
-    public boolean addDepartment(@RequestBody DepartmentDto departmentDto){
-        return departmentService.addDepartment(departmentDto);
+    public boolean addDepartment(@RequestBody Departments department){
+        return departmentService.addDepartment(department);
     }
     @PutMapping("/update")
-    public boolean updateDepartment(@RequestBody DepartmentDto departmentDto){
-        return departmentService.updateDepartment(departmentDto);
+    public boolean updateDepartment(@RequestBody Departments department){
+        return departmentService.updateDepartment(department);
     }
     @DeleteMapping("/delete/{id}")
     public boolean deleteDepartment(@PathVariable Integer id){
         return departmentService.deleteDepartment(id);
     }
     @GetMapping("/search/{id}")
-    public DepartmentDto searchDepartment(@PathVariable Integer id){
+    public Departments searchDepartment(@PathVariable String id){
         return departmentService.searchDepartment(id);
     }
 }
